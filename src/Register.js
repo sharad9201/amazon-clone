7d05732aecc63cc732d6a0049a9e5f114aa5fb43
sharad9201 <sharad.rae19@gmail.com>
2021-08-25 12:27:17 +0545

    user register with api

--- src/Register.js
+++ src/Register.js
@@ -1,8 +1,38 @@
+import { result } from 'lodash'
+import React, {useState} from 'react'
+
 function Register(){
 
-    return (
+    const [name, setName] = useState("")
+    const [password, setPassword] = useState("")
+    const [email, setEmail] = useState("")
+
+    async function signUp(){
+        let item = {name, password, email}
+        console.warn(item)
+
+        let result = await fetch("http://127.0.0.1:8000/api/register",{
+            method: 'POST',
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"appliction/json"
+            },
+            body:JSON.stringify(item)
+        })
+
+        result = await result.json()
+        console.warn("result", result)
+    }
+    return ( 
          <div className="">
-             <h1>Register page</h1>
+             <h1>User Sign up</h1>
+             <input type="text" value={name} onChange={(e)=>setName(e.target.value)} className="form-control" />
+             <br />
+             <input type="text" value={email} onChange={(e)=>setEmail(e.target.value)} className="form-control" />
+             <br />
+             <input type="text" value={password} onChange={(e)=>setPassword(e.target.value)} className="form-control" />
+             <br />
+             <button onClick={signUp} className="btn btn-primary"> Sign up </button>
           </div>
     )
 }
