6d444ae4630b698161b0665024c3af70931d0b82
sharad9201 <sharad.rae19@gmail.com>
2021-08-25 22:04:19 +0545

    logout cleared from local storage

--- src/Header.js
+++ src/Header.js
@@ -1,44 +1,45 @@
-import {Navbar,Nav,NavDropdown } from 'react-bootstrap'
-import {Link} from 'react-router-dom'
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link, useHistory } from "react-router-dom";
 
-function Header(){
-    //finds the user-name 
-    const user = JSON.parse(localStorage.getItem('user-info'))
-    console.log(user)
-return(
+function Header() {
+  const history = useHistory();
+  //finds the user-name
+  const user = JSON.parse(localStorage.getItem("user-info"));
+  console.log(user);
+
+  function logout() {
+    localStorage.clear();
+    history.push("/register");
+  }
+  return (
     <div className="mr-auto nav_bar_wrapper">
-    <Navbar bg="dark" variant="dark">
-       
+      <Navbar bg="dark" variant="dark">
         <Navbar.Brand href="#home">Navbar</Navbar.Brand>
         {/* protected routes after user info the login and resister is disables */}
-           <Nav className="me-auto">
-               {
-                   localStorage.getItem('user-info') ?
-                   <>
-                    <Link to="/add">Add Product</Link>
-                    <Link to="/update">Update Product</Link>
-                   </> :
+        <Nav className="me-auto">
+          {localStorage.getItem("user-info") ? (
+            <>
+              <Link to="/add">Add Product</Link>
+              <Link to="/update">Update Product</Link>
+            </>
+          ) : (
+            <>
+              <Link to="/login">Login</Link>
+              <Link to="/register">Register</Link>
+            </>
+          )}
+        </Nav>
 
-                   <>
-                    <Link to="/login">Login</Link>
-                    <Link to="/register">Register</Link>
-                   </>
-               }
-               
-               
-            </Nav>
-            <Nav>
-            <NavDropdown title={user.name}>
-                <NavDropdown.Item>LOgout</NavDropdown.Item>
-                <NavDropdown.Item>Profile</NavDropdown.Item>
-
-                </NavDropdown>
-            </Nav>
-           
-            </Navbar>
-        </div>
-
-)
-  
+        {localStorage.getItem("user-info") ? (
+          <Nav>
+            <NavDropdown title={user && user.name}>
+              <NavDropdown.Item onClick={logout}>LOgout</NavDropdown.Item>
+              <NavDropdown.Item>Profile</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+        ) : null}
+      </Navbar>
+    </div>
+  );
 }
-export default Header
+export default Header;
