2aaf588d1613e3b711f5eb2cf014a690c78e2b63
sharad9201 <sharad.rae19@gmail.com>
2021-08-26 22:12:28 +0545

    product list ad randon table

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,13 +1,50 @@
 import Header from "./Header";
+import React, { useState, useEffect } from "react";
+import {Table} from "react-bootstrap";
 
-function ProductList(){
+function ProductList() {
+  const [data, setData] = useState([]);
+  useEffect(async () => {
+    let result = await fetch("http://127.0.0.1:8000/api/list");
+    result = await result.json();
+    setData(result);
+  }, []);
+  console.warn(data);
 
-    return(
-        <div>
-            <Header />
-            <h1>Product List</h1>
-        </div>
-    )
+  return (
+    <div>
+      <Header />
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
+    </div>
+  );
 }
 
 export default ProductList;
