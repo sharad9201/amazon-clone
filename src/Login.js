79a51e4a5ba4dc992b3b692dbaf99ba199bfe1cd
sharad9201 <sharad.rae19@gmail.com>
2021-08-26 10:52:20 +0545

    login api

--- src/Login.js
+++ src/Login.js
@@ -3,6 +3,8 @@ import React, {useState, useEffect} from 'react'
 import {useHistory } from 'react-router-dom'
 
 function Login(){
+    const [email,setEmail] = useState("")
+    const [password,setPassword] = useState("")
 
     const history = useHistory()
 
@@ -11,11 +13,33 @@ function Login(){
             history.push('./add')
         }
     },[])
+    // button wala login 
+    async function logIn(){
+        let item = {email,password}
+        let result = await fetch("http://127.0.0.1:8000/api/login",{
+            method:"POST",
+            headers:{
+                "Content-Type":"application/json",
+                "Accept":"application/json"
+            },
+            body:JSON.stringify(item)
+        })
+        result = await result.json()
+        localStorage.setItem('user-info',JSON.stringify(result))
+        history.push('/add')
+    }
     return (
         <div className="">
                  <Header />
-
-
+                <div className="">
+                    <h1>User LOg up</h1>
+                   
+                    <input type="text" value={email} placeholder="email" onChange={(e)=>setEmail(e.target.value)} className="form-control" />
+                    <br />
+                    <input type="text" value={password} placeholder="password" onChange={(e)=>setPassword(e.target.value)} className="form-control" />
+                    <br />
+                    <button onClick={logIn} className="btn btn-primary"> Sign up </button>
+                </div>
         </div>
     )
 }
